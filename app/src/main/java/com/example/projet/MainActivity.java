50d729699a293318572577d9a3026b119f68909c
Al3xell <alexandre35137@gmail.com>
2022-04-11 17:20:23 +0200

    Merge everything

--- app/src/main/java/com/example/projet/MainActivity.java
+++ app/src/main/java/com/example/projet/MainActivity.java
@@ -3,14 +3,54 @@ package com.example.projet;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    private TextView textEmail;
+    private Button btn_disconnect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        btn_disconnect = findViewById(R.id.btnDisconnect);
+        btn_disconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                checkUser();
+            }
+        });
+
+
+    }
+
+    private void checkUser() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            startActivity(new Intent(MainActivity.this, FirebaseUI.class));
+            finish();
+        }
+        else {
+            String email = firebaseUser.getEmail();
+            textEmail = findViewById(R.id.txtEmail);
+            textEmail.setText(email);
+        }
     }
 }
